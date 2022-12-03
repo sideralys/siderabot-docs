7ac231ab816ee1833518fde5720e97bb3a9cc36e
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2022-12-03 14:49:41 +0100

    ci(build): use correct URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Siderabot',
   tagline: 'Documentation',
-  url: 'https://sideralys.github.io/siderabot-docs',
+  url: 'https://sideralys.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
